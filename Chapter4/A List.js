736b55ae91b79a1996288381298359193ac79dcc
uboat46 <uboat46@hotmail.com>
2017-07-21 17:32:00 -0500

    Finished Chapter 4

---
+++ Chapter4/A List.js
@@ -0,0 +1,51 @@
+// Write a function arrayToList that builds up a data structure like the previous one when given
+// [1, 2, 3] as argument, and write a listToArray function that produces an array from a list. 
+
+// Also write the helper functions prepend, which takes an element and a list and creates a new list that 
+// adds the element to the front of the input list, and nth, which takes a list and a number and returns the 
+// element at the given position in the list, or undefined when there is no such element.
+
+// If you haven’t already, also write a recursive version of nth.
+
+
+//CODE AUTHOR == UBOAT46
+
+
+function arrayToList(arr){
+    var list = {};
+    for(var i = arr.length -1; i >= 0; i--){
+        list = {value:arr[i], rest: list};
+    }
+    return list;
+}
+
+function listToArray(list){
+    var a = [];
+    for(var node = list; node; node = node.rest){
+        if(node.value)a.push(node.value);
+    }
+    return a;
+}
+
+function prepend(element, list){
+    return list = {value:element, rest:list};
+}
+
+function nth(list, number){//could be recursive
+    var i = 0;
+    var element;
+    for( var node = list; node; node = node.rest){
+        if(number == i)return node.value;
+        i++;
+    }
+}
+
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 3));
+// → 20
---
+++ Chapter4/Reversing An Array.js
@@ -0,0 +1,36 @@
+// Arrays have a method reverse, which changes the array by inverting the order in which its elements appear.
+// For this exercise, write two functions, reverseArray and reverseArrayInPlace. 
+
+// The first, reverseArray, takes an array as argument and produces a new array that has the same elements 
+// in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the 
+// array given as argument in order to reverse its elements. Neither may use the standard reverse method.
+
+//CODE AUTHOR == UBOAT46
+
+
+function reverseArray(arr){
+    var newArr = [];
+    for(var i = arr.length -1; i >= 0; i--){
+        newArr.push(arr[i]);
+    }
+    return newArr;
+}
+
+function reverseArrayInPlace(arr){
+    var i = 0;
+    var j = arr.length -1;
+    while(i <= j){
+        var x = arr[i];
+        arr[i] = arr[j];
+        arr[j] = x;
+        i++;
+        j--;
+    }
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
