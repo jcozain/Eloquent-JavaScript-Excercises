0128f4d918fb91b1ec0b34ba0303e1678b67eece
uboat46 <uboat46@hotmail.com>
2017-07-29 21:32:57 -0500

    Finished Chapter 6

--- Chapter6/Sequence Interface.js
+++ Chapter6/Sequence Interface.js
@@ -15,6 +15,55 @@
 
 //CODE AUTHOR = UBOAT46
 
+function ArraySeq(array){
+    this.pos = -1;
+    this.array = array;
+}
+
+ArraySeq.prototype.next = function(){
+    if(this.pos >= this.array.length - 1){
+        return false;
+    }
+    this.pos++;
+    return true;
+}
+
+ArraySeq.prototype.current = function(){
+    return this.array[this.pos];
+}
+
+function RangeSeq(from, to) {
+  this.pos = from - 1;
+  this.to = to;
+}
+
+RangeSeq.prototype.next = function() {
+  if (this.pos >= this.to){
+    return false;
+  }
+  this.pos++;
+  return true;
+};
+
+RangeSeq.prototype.current = function() {
+  return this.pos;
+};
+
+function logFive(sequence) {
+  for (var i = 0; i < 5; i++) {
+    if (!sequence.next()){
+      break;
+    }
+    console.log(sequence.current());
+  }
+}
+
+function logSeq(sequence){
+    while(sequence.next()){
+        console.log(sequence.current());
+    }
+}
+
 logFive(new ArraySeq([1, 2]));
 // → 1
 // → 2
@@ -24,3 +73,16 @@ logFive(new RangeSeq(100, 1000));
 // → 102
 // → 103
 // → 104
+
+logSeq(new ArraySeq([1,2,3,4]));
+// -> 1
+// -> 2
+// -> 3
+// -> 4
+
+logSeq(new RangeSeq(1,100));
+// -> 1
+// -> 2
+// -> 3
+// -> 4
+// -> ...
