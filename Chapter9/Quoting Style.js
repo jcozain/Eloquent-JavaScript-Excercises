8d3f69009488c82e3b12b3c68f476af61b1b20c8
uboat46 <uboat46@hotmail.com>
2017-07-30 02:00:39 -0500

    Finished Chapter 9

---
+++ Chapter9/Quoting Style.js
@@ -0,0 +1,13 @@
+// Imagine you have written a story and used single quotation marks throughout to mark pieces 
+// of dialogue. Now you want to replace all the dialogue quotes with double quotes, while keeping 
+// the single quotes used in contractions like aren’t.
+
+// Think of a pattern that distinguishes these two kinds of quote usage and craft a call to the 
+// replace method that does the proper replacement.
+
+//CODE AUTHOR = UBOAT46
+
+var text = "'I'm the cook,' he said, 'it's my job.'";
+// Change this call.
+console.log(text.replace(/(^|\W)'|'(\W|$)/g, '$1"$2'));
+// → "I'm the cook," he said, "it's my job."
